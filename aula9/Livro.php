a84cba66fe67414311f85c479d0b6be60f567058
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-12-11 14:05:54 -0300

    aula9

---
+++ aula9/Livro.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once './Pessoa.php';
+require_once './Publicacao.php';
+
+class Livro{
+    private $titulo;
+    private $autor;
+    private $totPaginas;
+    private $pagAtual;
+    private $aberto;
+    private $leitor;
+
+    function __construct($titulo, $autor, $totPaginas, $leitor){
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->totPaginas = $totPaginas;
+        $this->pagAtual = 0;
+        $this->aberto = false;
+        $this->leitor = $leitor;
+
+    }
+
+    public function getTitulo(){
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function getAutor(){
+        return $this->autor;
+    }
+
+    public function setAutor($autor){
+        $this->autor = $autor;
+    }
+
+    public function getTotPaginas(){
+        return $this->totPaginas;
+    }
+
+    public function setTotPaginas($totPaginas){
+        $this->totPaginas = $totPaginas;
+    }
+
+    public function getPagAtual(){
+        return $this->pagAtual;
+    }
+
+    public function setPagAtual($pagAtual){
+        $this->pagAtual = $pagAtual;
+    }
+
+    public function getAberto(){
+        return $this->aberto;
+    }
+
+    public function setAberto($aberto){
+        $this->aberto = $aberto;
+    }
+
+    public function getLeitor(){
+        return $this->leitor;
+    }
+
+    public function setLeitor($leitor){
+        $this->leitor = $leitor;
+    }
+
+    public function detalhes(){
+        echo "<hr>Livro {$this->titulo} escrito por {$this->autor}";
+        echo "<br/> Páginas {$this->totPaginas} atual {$this->pagAtual}";
+        echo "<br/> Sendo lido por {$this->leitor->getNome()}";
+    }
+
+    public function abrir(){
+        $this->aberto = true;
+    }
+
+    public function fechar(){
+        $this->aberto = false;
+    }
+
+    public function folhear($p){
+        if($p>$this->totPaginas){
+            $this->pagAtual = 0;
+        } else{
+            $this->pagAtual = $p;
+        }
+    }
+
+    public function avançarPagina(){
+        $this->pagAtual ++;
+    }
+
+    public function voltarPagina(){
+        $this->pagAtual --;
+    }
+}
+
+?>
